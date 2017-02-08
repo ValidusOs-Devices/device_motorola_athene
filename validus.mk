102968d7b4c5e6a4c4d252555d09df568322970c
Adam <ovadia.adam@gmail.com>
2017-02-08 14:33:02 -0500

    val plz

--- validus.mk
+++ validus.mk
@@ -14,8 +14,8 @@
 # limitations under the License.
 
 # Inherit some common Tesla stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
 
 $(call inherit-product, device/motorola/athene/full_athene.mk)
 
@@ -26,7 +26,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := athene
-PRODUCT_NAME := tesla_athene
+PRODUCT_NAME := validus_athene
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
 PRODUCT_RELEASE_NAME := athene
